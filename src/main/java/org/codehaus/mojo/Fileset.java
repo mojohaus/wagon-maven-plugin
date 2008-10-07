e27c314f87fea79caf5e9e1976e2b14443cf8c81
Dan Tran <dantran@gmail.com>
2008-10-07 03:17:26 +0000

    add file-management
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/sandbox/wagon-maven-plugin@7864 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/java/org/codehaus/mojo/Fileset.java
@@ -0,0 +1,51 @@
+package org.codehaus.mojo;
+
+import org.apache.maven.shared.model.fileset.FileSet;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+
+/**
+ * Customizes the string representation of
+ * <code>org.apache.maven.shared.model.fileset.FileSet</code> to return the
+ * included and excluded files from the file-set's directory. Specifically,
+ * <code>"file-set: <I>[directory]</I> (included: <I>[included files]</I>,
+ * excluded: <I>[excluded files]</I>)"</code>
+ *
+ * @version $Id$
+ * @since 1.0-alpha-1
+ * @see org.apache.maven.shared.model.fileset.FileSet
+ */
+public class Fileset
+    extends FileSet
+{
+    public static final long serialVersionUID = -1;
+
+    /**
+     * Retrieves the included and excluded files from this file-set's directory.
+     * Specifically, <code>"file-set: <I>[directory]</I> (included:
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>
+     *
+     * @return The included and excluded files from this file-set's directory.
+     * Specifically, <code>"file-set: <I>[directory]</I> (included:
+     * <I>[included files]</I>, excluded: <I>[excluded files]</I>)"</code>
+     * @see java.lang.Object#toString()
+     */
+    public String toString()
+    {
+        return "file-set: " + getDirectory() + " (included: " + getIncludes() + ", excluded: " + getExcludes() + ")";
+    }
+}
