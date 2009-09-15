762db42fd888a77304550616ef22d8a5d05c34c1
Dan Tran <dantran@gmail.com>
2009-09-15 00:06:26 +0000

    MOJO-1437: .ignore .indexer directory when merging
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/wagon-maven-plugin@10702 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/wagon/shared/DefaultMavenRepoMerger.java
+++ src/main/java/org/codehaus/mojo/wagon/shared/DefaultMavenRepoMerger.java
@@ -72,7 +72,7 @@ public class DefaultMavenRepoMerger
         WagonFileSet srcFileSet = new WagonFileSet();
         srcFileSet.setDownloadDirectory( downloadSrcDir );
         //ignore archiva/nexus .index at root dir
-        String[] excludes = { ".index/**" };
+        String[] excludes = { ".index/**", ".indexer/**" };
         srcFileSet.setExcludes( excludes );
 
         try
