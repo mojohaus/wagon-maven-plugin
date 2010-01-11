9ee9df9a1d671701ebb610140f0c9a3d1f4a9047
Dan Tran <dantran@gmail.com>
2010-01-11 08:31:19 +0000

    [MOJO-1488]  Ignore nexus' .meta at root directory for merge-maven-repo mojo
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/wagon-maven-plugin@11613 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/wagon/shared/DefaultMavenRepoMerger.java
+++ src/main/java/org/codehaus/mojo/wagon/shared/DefaultMavenRepoMerger.java
@@ -72,7 +72,7 @@ public class DefaultMavenRepoMerger
         WagonFileSet srcFileSet = new WagonFileSet();
         srcFileSet.setDownloadDirectory( downloadSrcDir );
         //ignore archiva/nexus .index at root dir
-        String[] excludes = { ".index/**", ".indexer/**" };
+        String[] excludes = { ".index/**", ".indexer/**, .meta/**" };
         srcFileSet.setExcludes( excludes );
 
         try
