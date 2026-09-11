39ff008ae1861f785fa024561e2f04d668714d4d
Sylwester Lachiewicz <slachiewicz@apache.org>
2026-09-11 22:14:52 +0200

    Site checkin for project Wagon Maven Plugin

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.wagon"},{"l":"org.codehaus.mojo.wagon_maven_plugin"},{"l":"org.codehaus.mojo.wagon.shared"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"org.codehaus.mojo.wagon"},{"l":"org.codehaus.mojo.wagon_maven_plugin"},{"l":"org.codehaus.mojo.wagon.shared"}];updateSearchResults();
